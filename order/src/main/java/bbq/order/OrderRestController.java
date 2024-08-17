e5cee5d47ec2005c749ad3ed166268329f96d436
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-17 20:26:40 +0200

    Final rest stand

--- order/src/main/java/bbq/order/OrderRestController.java
+++ order/src/main/java/bbq/order/OrderRestController.java
@@ -1,54 +1,41 @@
 package bbq.order;
 
 import bbq.order.model.Order;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @Tag(name = "Order", description = "Order Resource")
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderRestController {
 
     private final OrderRepository orderRepository;
 
-    private final OrderRestTemplatePublisher publisher;
+    private final OrderRestClientPublisher publisher;
 
-    private final Counter orderCounter;
-
-    public OrderRestController(
-            OrderRepository orderRepository,
-            OrderRestTemplatePublisher publisher,
-            MeterRegistry registry) {
-        this.orderRepository = orderRepository;
-        this.publisher = publisher;
-        this.orderCounter = Counter.builder("orders.placed")
-                .description("Number of orders placed")
-                .register(registry);
+    @GetMapping("/crash")
+    public String crash() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Found nothing...");
+        //throw new IllegalArgumentException("Don't get it.");
+//        throw new RuntimeException("Bam!");
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Order post(
-            JwtAuthenticationToken principal,
-            @RequestHeader(value = "Authorization") String authHeader,
-            @Valid @RequestBody Order order
-    ) {
-        try {
-            // 1. Save Order
-            var savedOrder = orderRepository.save(order);
+    public Order post(@Valid @RequestBody Order order) {
+        // Save Order
+        //return orderRepository.save(order);
+        var savedOrder = orderRepository.save(order);
 
-            // 2. Publish order
-            publisher.publish(savedOrder);
+        // 2. Publish order
+        publisher.publish(savedOrder);
 
-            return savedOrder;
-        } finally {
-            orderCounter.increment();
-        }
+        return savedOrder;
     }
 
 }
