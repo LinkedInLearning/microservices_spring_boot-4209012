4ff8c089cf2509023fd39775577d7762e07dd8b1
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-18 19:13:10 +0200

    Cleanup

--- order/src/main/java/bbq/order/OrderRepository.java
+++ order/src/main/java/bbq/order/OrderRepository.java
@@ -1,15 +1,7 @@
 package bbq.order;
 
 import bbq.order.model.Order;
-import com.github.kkuegler.HumanReadableIdGenerator;
-import com.github.kkuegler.PermutationBasedHumanReadableIdGenerator;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 public interface OrderRepository extends CrudRepository<Order, String> {
 
