0b29f6fcf7652ab8aaff0837734f24c3f0c58d20
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-18 18:50:30 +0200

    DB stuff

--- order/src/main/java/bbq/order/config/DummyOrderProducerRunner.java
+++ order/src/main/java/bbq/order/config/DummyOrderProducerRunner.java
@@ -1,6 +1,5 @@
 package bbq.order.config;
 
-import bbq.order.model.Cart;
 import bbq.order.model.Order;
 import com.github.kkuegler.PermutationBasedHumanReadableIdGenerator;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +8,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.util.Collections;
 
 @Component
 @Slf4j
@@ -25,10 +21,6 @@ public class DummyOrderProducerRunner implements ApplicationRunner {
         log.info("Sending dummy order for queue setup");
         var order = new Order();
         order.setId(new PermutationBasedHumanReadableIdGenerator().generate());
-        var cart = new Cart();
-        cart.setTotal(BigDecimal.TEN);
-        cart.setItems(Collections.emptyList());
-        order.setCart(cart);
         rabbitTemplate.convertAndSend("orders", "",  order);
     }
 }
