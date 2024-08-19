ce71c444d65a5a41a3456ba586a34a5a65cae935
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-19 18:07:06 +0200

    Prep Menu Rest

---
+++ order/src/main/java/bbq/order/MenuRestController.java
@@ -0,0 +1,23 @@
+package bbq.order;
+
+import bbq.order.model.MenuCategory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/menu")
+@RequiredArgsConstructor
+public class MenuRestController {
+
+    private final MenuRepository menuRepository;
+
+    @GetMapping
+    public List<MenuCategory> get() {
+        return menuRepository.findAll();
+    }
+
+}
