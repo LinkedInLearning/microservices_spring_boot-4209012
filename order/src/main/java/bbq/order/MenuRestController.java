40e48c8a014921408e01da8222cf058f2358ab31
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-19 17:35:51 +0200

    Removed respons entity

--- order/src/main/java/bbq/order/MenuRestController.java
+++ order/src/main/java/bbq/order/MenuRestController.java
@@ -3,7 +3,6 @@ package bbq.order;
 import bbq.order.model.MenuCategory;
 import bbq.order.model.MenuItem;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,15 +23,6 @@ public class MenuRestController {
     @GetMapping("/{key}/menu-items")
     public List<MenuItem> getByKey(@PathVariable String key, @RequestParam Optional<String> sort) {
         return menuRepository.findItemsByCategoryKey(key, sort);
-    }
-
-    @GetMapping("/response-entity")
-    public ResponseEntity<List<MenuCategory>> getResponseEntity() {
-        var menuCategories = menuRepository.findAll();
-        if (menuCategories.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(menuCategories);
     }
 
 }
