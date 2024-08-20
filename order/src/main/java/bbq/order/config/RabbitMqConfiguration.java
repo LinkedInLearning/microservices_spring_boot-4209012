4e3ba25d1e11c01ce33db0268aaa8a479625b900
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-20 11:37:07 +0200

    s

---
+++ order/src/main/java/bbq/order/config/RabbitMqConfiguration.java
@@ -0,0 +1,10 @@
+package bbq.order.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class RabbitMqConfiguration {
+    // TODO
+}
