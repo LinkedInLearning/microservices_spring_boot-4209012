425765f40e10bb275e9dc798a6aa120724e758b7
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-20 13:08:23 +0200

    x

--- order/src/main/java/bbq/order/config/RabbitMqConfiguration.java
+++ order/src/main/java/bbq/order/config/RabbitMqConfiguration.java
@@ -6,5 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 @Configuration
 public class RabbitMqConfiguration {
-    // TODO
+
+    /**
+     @Bean public Jackson2JsonMessageConverter messageConverter() {
+     var messageConverter = new Jackson2JsonMessageConverter();
+     messageConverter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
+     return messageConverter;
+     }
+
+     @Bean public Declarables rabbitDeclarables() {
+     return new Declarables();
+     }
+     */
+
 }
