cc54c4a46fc0cd8316a53829985da166cf471762
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-20 15:56:35 +0200

    More compile errors

--- order/src/main/java/bbq/order/HelloRestController.java
+++ order/src/main/java/bbq/order/HelloRestController.java
@@ -1,46 +1,22 @@
 package bbq.order;
 
-import bbq.order.model.Cart;
-import bbq.order.model.CartItem;
-import bbq.order.model.Order;
 import com.github.kkuegler.HumanReadableIdGenerator;
 import com.github.kkuegler.PermutationBasedHumanReadableIdGenerator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class HelloRestController {
 
-    private final KafkaTemplate<String, Order> kafkaTemplate;
-
     private final HumanReadableIdGenerator idGenerator = new PermutationBasedHumanReadableIdGenerator();
 
     @GetMapping
     public String get() {
         return "Hello Order!";
-    }
-
-    @GetMapping("/send")
-    public String send() {
-        var order = new Order();
-        //order.setId(idGenerator.generate());
-        var cart = new Cart();
-        cart.setTotal(BigDecimal.TEN);
-        var cartItem = new CartItem();
-        cartItem.setItemTitle("Burger");
-        cartItem.setItemPrice(BigDecimal.TEN);
-        cart.setItems(List.of(cartItem));
-//        order.setCart(cart);
-        kafkaTemplate.send("orders", order);
-        return "Done";
     }
 
 }
