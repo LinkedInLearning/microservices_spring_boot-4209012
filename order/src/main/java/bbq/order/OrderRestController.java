de89ffd74df0c57410542f63ad0e3558d64b6213
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-20 16:28:09 +0200

    x

--- order/src/main/java/bbq/order/OrderRestController.java
+++ order/src/main/java/bbq/order/OrderRestController.java
@@ -1,8 +1,6 @@
 package bbq.order;
 
 import bbq.order.model.Order;
-import com.github.kkuegler.HumanReadableIdGenerator;
-import com.github.kkuegler.PermutationBasedHumanReadableIdGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +11,6 @@ import org.springframework.web.bind.annotation.*;
 public class OrderRestController {
 
     private final OrderRepository orderRepository;
-
-    private final HumanReadableIdGenerator idGenerator = new PermutationBasedHumanReadableIdGenerator();
-
 
     @GetMapping
     public Iterable<Order> getOrders() {
