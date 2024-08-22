f00c60ee14925c458f382f94ebb9ce62eb0be574
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-22 15:30:34 +0200

    Challenge end

---
+++ order/src/main/java/bbq/order/config/SecurityConfigurationLocal.java
@@ -0,0 +1,27 @@
+package bbq.order.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("local")
+@EnableWebSecurity
+public class SecurityConfigurationLocal {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+}
