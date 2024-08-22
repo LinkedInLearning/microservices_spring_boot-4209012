030fea014d0b1579341d23c4e01ac8ca2fcd8306
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-22 15:43:16 +0200

    x

--- order/src/main/java/bbq/order/config/SecurityConfiguration.java
+++ order/src/main/java/bbq/order/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package bbq.order.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile("production")
 public class SecurityConfiguration {
 
     @Value("${spring.security.oauth2.resourceserver.issuer-uri}")
