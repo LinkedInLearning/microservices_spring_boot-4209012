82cf99235fe138591b549b3f0311f8b5f07ba259
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-23 08:31:13 +0200

    Update OrderListener.java

--- delivery/src/main/java/bbq/delivery/OrderListener.java
+++ delivery/src/main/java/bbq/delivery/OrderListener.java
@@ -3,8 +3,6 @@ package bbq.delivery;
 import bbq.delivery.model.Order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +22,5 @@ public class OrderListener {
         deliveryRepository.addNewOrder(order);
         log.info("add order: {}", order);
     }
-
-    @DltHandler
-    public void dltListener(
-            ConsumerRecord<String, String> message
-    ) {
-        log.info("DLT Message Received - key: {} , value: {}", message.key(), message.value());
-    }
-
+    
 }
