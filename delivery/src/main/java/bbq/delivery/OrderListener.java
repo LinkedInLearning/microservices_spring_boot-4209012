cf578931837819431026aa1c9190205871d4316b
Jan Stamer <914776+remast@users.noreply.github.com>
2024-08-23 08:25:58 +0200

    x

--- delivery/src/main/java/bbq/delivery/OrderListener.java
+++ delivery/src/main/java/bbq/delivery/OrderListener.java
@@ -3,6 +3,8 @@ package bbq.delivery;
 import bbq.delivery.model.Order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +17,19 @@ public class OrderListener {
 
     @KafkaListener(topics = "orders", groupId = "delivery", properties = {"spring.json.value.default.type=bbq.delivery.model.Order"})
     public void onOrder(Order order) {
-        if (true) throw new RuntimeException("Crash!");
         log.info("receive order: {}", order);
+
+        if (true) throw new RuntimeException("Crash!");
+
         deliveryRepository.addNewOrder(order);
+        log.info("add order: {}", order);
+    }
+
+    @DltHandler
+    public void dltListener(
+            ConsumerRecord<String, String> message
+    ) {
+        log.info("DLT Message Received - key: {} , value: {}", message.key(), message.value());
     }
 
 }
